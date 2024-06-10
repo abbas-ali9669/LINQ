146038456a1cb1565ce27dce2673ede45b5b5708
Abbas Ali <abbas.ali@veroke.com>
2024-06-10 22:18:43 +0500

    Add project files.

---
+++ LINQ/Introduction_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Introduction_1
+    {
+        // LINQ stands for Languaage Integrated Query.
+        // It is a part of the language but not a complete language.
+        // Linq is developed by Microsoft and is available in System.Linq namespace.
+        // LINQ is a uniform query syntax which can be applied on different data sources.
+        // LINQ offers a compact, expressive, and intelligible syntax for manipulating data.
+        // LINQ was introduced in .NET Framework 3.5 and C# 3.0.
+
+        // By using LINQ we can communicate with various data sources without learning the specific query.
+        // No need to learn a new quary languages for different data sources.
+        // Less code as compare to traditional approach.
+        // Compile time safety.
+        // Intellisense support.
+        // Has alots of in built methods to support different operations.
+        // If you are a C#, VB.Net develper (Web, Mobile, Console, chatbot etc), then you must learn LINQ.
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // So From this lecture series we are started learning LINQ(Language Integrated Queries).
+            Console.WriteLine("Hello World");
+        }
+    }
+}
